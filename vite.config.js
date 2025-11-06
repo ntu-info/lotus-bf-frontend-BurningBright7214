10cf200a94c7385490d1c93b17f6c00d45417c7d
ntu-info student <student@example.com>
2025-11-06 11:19:35 +0800

    Update vite config for flexible deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  // 如果部署到專案頁面，取消下面這行的註解並修改 repo 名稱
+  // base: '/lotus-bf-frontend-BurningBright7214/',
+  // 如果部署到個人 GitHub Pages (username.github.io)，使用下面這行
   base: '/',
 
   plugins: [react()],
